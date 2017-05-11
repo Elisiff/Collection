c565d6afe548cf7b41b8d9b665424d6ed53d76a0
Anastasiya Vasina <nast791@mail.ru>
2017-05-11 15:15:46 +0300

    Фигуры

--- sass/blocks/lib-block.js
+++ sass/blocks/lib-block.js
@@ -1,14 +1,16 @@
-let spoiler = document.querySelector(".lib-block__spoiler");
-let example = document.querySelector(".lib-block__example");
+var spoiler = document.querySelectorAll(".lib-block__spoiler");
+var example = document.querySelectorAll(".lib-block__example");
 
-if (spoiler !== null) {
-  spoiler.addEventListener("click", function() {
-    if (example.classList.contains("lib-block__example--closed")) {
-      example.classList.remove("lib-block__example--closed");
-      example.classList.add("lib-block__example--show");
-    } else if (example.classList.contains("lib-block__example--show")) {
-      example.classList.remove("lib-block__example--show");
-      example.classList.add("lib-block__example--closed");
+for (var i = 0; i < spoiler.length; i++) {
+  spoiler[i].addEventListener("click", function() {
+    for (var k = 0; k < example.length; k++) {
+      if (example[k].classList.contains("lib-block__example--closed")) {
+        example[k].classList.remove("lib-block__example--closed");
+        example[k].classList.add("lib-block__example--show");
+      } else if (example[k].classList.contains("lib-block__example--show")) {
+        example[k].classList.remove("lib-block__example--show");
+        example[k].classList.add("lib-block__example--closed");
+      }
     }
   });
 }
